90e41b760a4a232c0be827655e5f9c2ba63d22bf
Milena-Goncalves <g.francamilena@gmail.com>
2023-02-21 17:14:18 +0000

    frist commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,39 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import "./App.css";
+import bruxa from "./img/bruxa.png";
+import fada from "./img/fada.png";
+import fantasma from "./img/fantasma.png";
+import pirata from "./img/pirata.png";
+import vampiro from "./img/vampiro.png";
+import zumbi from "./img/zumbi.png";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  state = {
+    carnaval: [
+      { imagem: `${bruxa}`, titulo: "Bruxa" },
+      { imagem: `${fantasma}`, titulo: "Fantasma" },
+      { imagem: `${vampiro}`, titulo: "Vampiro" },
+      { imagem: `${zumbi}`, titulo: "Zumbi" },
+      { imagem: `${fada}`, titulo: "Fada" },
+      { imagem: `${pirata}`, titulo: "Pirata" },
+    ],
+  };
+
+  render() {
+    return (
+      <div className="principal">
+        <h1>Carnaval VnW</h1>
+        <section className="state-carnaval">
+          {this.state.carnaval.map((item) => (
+            <div className="carnaval">
+              <img src={item.imagem} alt={item.titulo} />
+              <h2>{item.titulo}</h2>
+            </div>
+          ))}
+        </section>
+      </div>
+    );
+  }
 }
 
 export default App;
